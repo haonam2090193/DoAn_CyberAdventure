694e989826bb3158553d8f55086eea0ef6882c35
MAY14\MAY14 <thcsthg.haonam@gmail.com>
2023-04-20 21:31:03 +0700

    [readd] Patrol P1

--- Assets/Scripts/Enemy/EnemyPatrol.cs
+++ Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -4,15 +4,78 @@ using UnityEngine;
 
 public class EnemyPatrol : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Animator anim;
 
-    // Update is called once per frame
+    [Header("Patrol Point")]
+    [SerializeField] private Transform leftEdge;
+    [SerializeField] private Transform rightEdge;
+
+    [Header("Enemy")]
+    [SerializeField] private Transform enemy;
+
+    [Header("Idle Time")]
+    [SerializeField] private float idleTimer;
+    [SerializeField] private float idleDuration;
+
+    [Header("Movement")]
+    [SerializeField] private float speed;
+    private Vector3 initScale;
+    private bool movingLeft;
+
+    private void Awake()
+    {
+        initScale = enemy.localScale;
+    }
     void Update()
     {
-        
+        if (movingLeft)
+        {
+            if (enemy.position.x >= leftEdge.position.x)
+            {
+                MoveInDirection(-1);
+            }
+            else
+            {
+                DirectionChange();
+            }
+        }
+        else
+        {
+            if (enemy.position.x <= rightEdge.position.x)
+            {
+                MoveInDirection(1);
+
+            }
+            else
+            {
+                DirectionChange();
+            }
+        }
     }
+    private void OnDisable()
+    {
+        anim.SetBool("moving", false);
+    }
+    private void MoveInDirection(int _direction)
+    {
+        idleTimer = 0;
+        anim.SetBool("moving",true);
+
+        enemy.localScale = new Vector3(Mathf.Abs(initScale.x) * _direction, 
+            initScale.y, initScale.x);
+
+        enemy.position = new Vector3(enemy.position.x + Time.deltaTime * _direction * speed,
+            enemy.position.y, enemy.position.z);
+    }
+    private void DirectionChange()
+    {
+        anim.SetBool("moving", false);
+
+        idleTimer += Time.deltaTime;
+
+        if(idleTimer > idleDuration)
+            movingLeft = !movingLeft;
+    }
+    
+    
 }
